efc2b8c89fd8a19f9a24ed97bc2aebe26aa82cdf
Gaoxjcn <gaoxj040620@outlook.com>
2023-04-11 12:53:04 +0800

    Update 物理实验假数据.py
    fix

--- 物理实验假数据.py
+++ 物理实验假数据.py
@@ -10,21 +10,21 @@ while True:
         if a==0:
             break
         num=[]
-        adv=input("输入原始数据（输入Re重置精确到小数点后位数） ")
-        if adv in ['re','Re','RE','rE']:
+        ave=input("输入原始数据（输入Re重置精确到小数点后位数） ")
+        if ave in ['re','Re','RE','rE']:
             break
-        adv=eval(adv)
-        adv*=pow(10,a)
-        n=eval(input("你想生成数据的个数 "))
+        ave=eval(ave)
+        ave*=pow(10,a)
+        n=int(input("你想生成数据的个数 "))
         if n%2==1:
             n-=1
-            num.append(adv)
-        for i in range(n):
-            temp=random.randint(1,9)
-            num.append(adv+temp)
-            num.append(adv-temp)
-        random.shuffle(num)
+            num.append(ave)
         for i in range(n//2):
+            temp=random.randint(1,9)
+            num.append(ave+temp)
+            num.append(ave-temp)
+        random.shuffle(num)
+        for i in range(n):
             num[random.randint(0,len(num)-1)]+=1
             num[random.randint(0,len(num)-1)]-=1
         for i in num:
